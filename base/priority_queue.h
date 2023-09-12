656aa76b6bc68b85423ef989ab07c11706ef6c42
lipingan <lipingan.dev@outlook.com>
2023-09-12 22:18:44 +0800

    Add MaxHeap.

--- base/priority_queue.h
+++ base/priority_queue.h
@@ -44,5 +44,64 @@ public:
 private:
   std::list<T> list_;
 };
+
+template <typename T> class MaxHeap {
+public:
+  explicit MaxHeap(std::size_t size) : nodes_(new T[size + 1]) {}
+
+  virtual ~MaxHeap() { delete[] nodes_; }
+
+  bool Empty() const { return size_ == 0; }
+
+  std::size_t Size() const { return size_; }
+
+  void Insert(T const &t) {
+    nodes_[++size_] = t;
+    Swim(size_);
+  }
+
+  T PopMax() {
+    T max = nodes_[1];
+    Exchange(1, size_--);
+    nodes_[size_ + 1] = {};
+    Sink(1);
+    return max;
+  }
+
+private:
+  bool Less(std::size_t i, std::size_t j) { return nodes_[i] < nodes_[j]; }
+
+  void Exchange(std::size_t i, std::size_t j) {
+    T t = nodes_[i];
+    nodes_[i] = nodes_[j];
+    nodes_[j] = t;
+  }
+
+  void Swim(std::size_t i) {
+    while (i > 1 && Less(i / 2, i)) {
+      Exchange(i / 2, i);
+      i = i / 2;
+    }
+  }
+
+  void Sink(std::size_t i) {
+    while (2 * i <= size_) {
+      int j = 2 * i;
+      if (j < size_ && Less(j, j + 1)) {
+        ++j;
+      }
+      if (!Less(i, j)) {
+        break;
+      }
+      Exchange(i, j);
+      i = j;
+    }
+  }
+
+  T *nodes_;
+  std::size_t size_ = 0;
+};
+
+template <typename T> class MinHeap {};
 } // namespace base
 #endif // BASE_PRIORITY_QUEUE_H_
--- base/priority_queue_tests.cc
+++ base/priority_queue_tests.cc
@@ -20,9 +20,22 @@ TEST(PriorityQueue, case0) {
     priority_queue.Insert(i);
   }
   while (!priority_queue.Empty()) {
-    std::cout << priority_queue.Max() << std::endl;
+    std::cout << priority_queue.Max() << ", ";
     priority_queue.PopMax();
   }
+  std::cout << std::endl;
   ASSERT_EQ(priority_queue.Empty(), true);
+}
+
+TEST(MaxHeap, case0) {
+  MaxHeap<int> max_heap(10);
+  std::vector<int> vector = {12, 22, 34, 14, 45, 62, 17, 84, 96};
+  for (auto &&i : vector) {
+    max_heap.Insert(i);
+  }
+  while (!max_heap.Empty()) {
+    std::cout << max_heap.PopMax() << ", ";
+  }
+  std::cout << std::endl;
 }
 } // namespace base
