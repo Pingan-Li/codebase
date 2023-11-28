399e96c7126ff46d00d15b091dc4034ba3463bba
lipingan <lipingan.dev@outlook.com>
2023-11-28 10:06:15 +0800

    Update thread_printing.cc

--- experiment/thread_printing.cc
+++ experiment/thread_printing.cc
@@ -11,6 +11,7 @@
 #include <condition_variable>
 #include <iostream>
 #include <mutex>
+#include <thread>
 
 namespace a {
 std::mutex mtx;
@@ -92,7 +93,127 @@ void a() {
 }
 } // namespace a
 
+namespace b {
+
+std::mutex mtx;
+std::condition_variable cv;
+inline constexpr int kMax = 100;
+int count = 0;
+
+void print(int value) {
+  while (true) {
+    {
+      std::unique_lock<std::mutex> unique_lock{mtx};
+      if (!(count < kMax))
+        break;
+      while (count % 4 != value) {
+        cv.wait(unique_lock);
+      }
+      std::cout << "thread " << (count % 4) << ", " << 'A' + (count % 4)
+                << "\n";
+      ++count;
+    }
+    cv.notify_all();
+  }
+}
+
+void b() {
+  std::thread thread3{print, 3};
+  std::thread thread2{print, 2};
+  std::thread thread1{print, 1};
+  std::thread thread0{print, 0};
+
+  thread0.join();
+  thread1.join();
+  thread2.join();
+  thread3.join();
+}
+} // namespace b
+
+namespace c {
+std::mutex mtx;
+std::condition_variable cv;
+inline constexpr int kMax = 100;
+int counter = 0;
+
+void print_a() {
+  while (true) {
+    std::unique_lock<std::mutex> unique_lock{mtx};
+    if (!(counter < kMax))
+      break;
+    while (!(counter % 4 == 0)) {
+      cv.wait(unique_lock);
+    }
+    std::cout << "thread 0, print: A\n";
+    ++counter;
+    unique_lock.unlock();
+    // notify_all() is required.
+    cv.notify_all();
+  }
+}
+
+void print_b() {
+  while (true) {
+    std::unique_lock<std::mutex> unique_lock{mtx};
+    if (!(counter < kMax))
+      break;
+    while (!(counter % 4 == 1)) {
+      cv.wait(unique_lock);
+    }
+    std::cout << "thread 1, print: B\n";
+    ++counter;
+    unique_lock.unlock();
+    // notify_one() is enough.
+    cv.notify_all();
+  }
+}
+
+void print_c() {
+  while (true) {
+    std::unique_lock<std::mutex> unique_lock{mtx};
+    if (!(counter < kMax))
+      break;
+    while (!(counter % 4 == 2)) {
+      cv.wait(unique_lock);
+    }
+    std::cout << "thread 2, print: C\n";
+    ++counter;
+    unique_lock.unlock();
+    cv.notify_all();
+  }
+}
+
+void print_d() {
+  while (true) {
+    std::unique_lock<std::mutex> unique_lock{mtx};
+    if (!(counter < kMax))
+      break;
+    while (!(counter % 4 == 3)) {
+      cv.wait(unique_lock);
+    }
+    std::cout << "thread 3, print: D\n";
+    ++counter;
+    unique_lock.unlock();
+    cv.notify_all();
+  }
+}
+
+void c() {
+  std::thread thread0{print_a};
+  std::thread thread1{print_b};
+  std::thread thread2{print_c};
+  std::thread thread3{print_d};
+
+  thread0.join();
+  thread1.join();
+  thread2.join();
+  thread3.join();
+}
+} // namespace c
+
 int main(int argc, char **argv) {
   a::a();
+  b::b();
+  c::c();
   return 0;
 }
