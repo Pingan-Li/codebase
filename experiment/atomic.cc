99db938e59d18c153b446f525e97e17a01161b7e
LiPingan <lipingan.dev@outlook.com>
2023-12-17 22:48:12 +0800

    Update the atomic experiment

--- experiment/atomic.cc
+++ experiment/atomic.cc
@@ -9,46 +9,51 @@
  *
  */
 
-#include "testing/googletest/include/gtest/gtest.h"
 #include <atomic>
 #include <thread>
+#include <vector>
 
-constexpr int count = 1000000;
+#include "testing/googletest/include/gtest/gtest.h"
+
+static constexpr int kMax = 1000000;
+static constexpr int kThreadCount = 40;
 
 int volatile value = 0;
-void Accumulate(int volatile *value) {
-  for (int i = 0; i < count; ++i) {
-    *value = *value + 1;
+std::atomic_int atomic_value = 0;
+
+void Accumulate() {
+  for (int i = 0; i < kMax; ++i) {
+    value = value + 1;
   }
 }
 
 TEST(Volatile, Accumulate) {
-  std::thread t1(Accumulate, &value);
-  std::thread t2(Accumulate, &value);
-  t1.join();
-  t2.join();
-  ASSERT_EQ(value, 2 * count);
+  std::vector<std::thread> worker_threads;
+  for (int i = 0; i < kThreadCount; ++i) {
+    worker_threads.emplace_back(Accumulate);
+  }
+
+  for (auto &&thread : worker_threads) {
+    thread.join();
+  }
+
+  ASSERT_LT(value, kThreadCount * kMax);
 }
 
-std::atomic_int atomic_value = 0;
-void AtomicAccumulate(std::atomic_int &value) {
-  for (int i = 0; i < count; ++i) {
-    ++atomic_value;
+void AtomicAccumulate() {
+  for (int i = 0; i < kMax; ++i) {
+    atomic_value.fetch_add(1, std::memory_order_relaxed);
   }
 }
 
 TEST(Atomic, Accumulate) {
-  std::thread t1(AtomicAccumulate, std::ref(atomic_value));
-  std::thread t2(AtomicAccumulate, std::ref(atomic_value));
-  std::thread t3(AtomicAccumulate, std::ref(atomic_value));
-  std::thread t4(AtomicAccumulate, std::ref(atomic_value));
-  std::thread t5(AtomicAccumulate, std::ref(atomic_value));
-  std::thread t6(AtomicAccumulate, std::ref(atomic_value));
-  t1.join();
-  t2.join();
-  t3.join();
-  t4.join();
-  t5.join();
-  t6.join();
-  ASSERT_EQ(atomic_value, 6 * count);
+  std::vector<std::thread> worker_threads;
+  for (int i = 0; i < kThreadCount; ++i) {
+    worker_threads.emplace_back(AtomicAccumulate);
+  }
+
+  for (auto &&thread : worker_threads) {
+    thread.join();
+  }
+  ASSERT_EQ(atomic_value, kThreadCount * kMax);
 }
