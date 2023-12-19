742793cf259180470e9b4861a6cd9e91768b8948
lipingan <lipingan.dev@outlook.com>
2023-12-19 09:42:01 +0800

    Update CMakeLists.txt and cmake-setup.sh

--- cmake-setup.sh
+++ cmake-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cmake -S ./ -G Ninja -B out/Debug -DCMAKE_BUILD_TYPE=Debug
-cmake -S ./ -G Ninja -B out/Release -DCMAKE_BUILD_TYPE=Release
-cmake -S ./ -G Ninja -B out/RelWithDebInfo -DCMAKE_BUILD_TYPE=RelWithDebInfo
-cmake -S ./ -G Ninja -B out/MinSizeRel -DCMAKE_BUILD_TYPE=MinSizeRel
+cmake -S ./ -G Ninja -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -B out/Debug -DCMAKE_BUILD_TYPE=Debug
+cmake -S ./ -G Ninja -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -B out/Release -DCMAKE_BUILD_TYPE=Release
+cmake -S ./ -G Ninja -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -B out/RelWithDebInfo -DCMAKE_BUILD_TYPE=RelWithDebInfo
+cmake -S ./ -G Ninja -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -B out/MinSizeRel -DCMAKE_BUILD_TYPE=MinSizeRel
