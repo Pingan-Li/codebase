be7d077f53a4cdff0f6a0abb94ed183d0aef02e6
lipingan <lipingan.dev@outlook.com>
2023-12-26 00:32:42 +0800

    Update reference_counting_exp.cc

--- experiment/reference_counting_exp.cc
+++ experiment/reference_counting_exp.cc
@@ -11,11 +11,12 @@
 
 #include <cassert>
 #include <cstddef>
+#include <cstdint>
 #include <iostream>
 #include <utility>
 
 namespace base {
-template <typename Type> class Pointer {
+template <typename Type> class Reference {
 public:
   using value_type = Type;
   using pointer = value_type *;
@@ -23,11 +24,71 @@ public:
   using reference = value_type &;
   using const_reference = value_type const &;
 
-  Pointer() noexcept : pointer_(nullptr) {}
+  /**
+   * @brief Default constructor of Reference.
+   *
+   */
+  Reference() noexcept = default;
 
-  explicit Pointer(std::nullptr_t) noexcept : pointer_(nullptr) {}
+  /**
+   * @brief Construct a new Reference object
+   *
+   * @param ptr
+   */
+  explicit Reference(pointer ptr) : pointer_(ptr) {
+    if (pointer_) {
+      pointer_->Increase();
+    }
+  }
 
-  explicit Pointer(pointer ptr) : pointer_(ptr) {}
+  explicit Reference(const_pointer const_ptr) : pointer_(const_ptr) {
+    if (pointer_) {
+      pointer_->Increase();
+    }
+  }
+
+  template <typename... Args>
+  Reference(Args &&...args) : pointer_(new Type(std::forward<Args>(args)...)) {}
+
+  explicit Reference(std::nullptr_t) noexcept {}
+
+  bool operator==(std::nullptr_t) noexcept { return pointer_ == nullptr; }
+
+  /**
+   * @brief Copy constructor
+   *
+   * @param other
+   */
+  Reference(Reference const &other) : pointer_(other.pointer_) {
+    if (pointer_) {
+      pointer_->Increase();
+    }
+  }
+  Reference &operator=(Reference const &other) {
+    if (this != &other) {
+      if (pointer_) {
+        pointer_->Decrease();
+      }
+      pointer_ = other.pointer_;
+      if (pointer_) {
+        pointer_->Increase();
+      }
+    }
+    return *this;
+  }
+  Reference(Reference &&other) : pointer_(other.pointer_) {
+    other.pointer_ = nullptr;
+  }
+  Reference &operator=(Reference &&other) {
+    pointer_ = other.pointer_;
+    other.pointer_->Decrease();
+    other.pointer_ = nullptr;
+  }
+  ~Reference() {
+    if (pointer_ && pointer_->Decrease()) {
+      std::cout << "Bye-bye~" << std::endl;
+    }
+  }
 
   pointer operator->() noexcept { return pointer_; }
 
@@ -37,56 +98,64 @@ public:
 
   const_reference operator*() const noexcept { return *pointer_; }
 
-  bool operator==(nullptr_t) const noexcept { return pointer_ == nullptr; }
+  bool operator==(std::nullptr_t) const noexcept { return pointer_ == nullptr; }
 
-  template <typename U> bool operator==(Pointer<U> ptr) {
+  template <typename U> bool operator==(Reference<U> ptr) {
     return pointer_ == ptr.pointer_;
   }
 
 private:
-  pointer pointer_;
+  pointer pointer_ = nullptr;
 };
-
-template <typename Type> struct CountingTraits {};
 
 template <typename Type> class RefCnt {
 public:
   RefCnt() noexcept : cnt_(1) {}
 
+  explicit RefCnt(std::int32_t initial_value) noexcept : cnt_(initial_value) {}
+
   void Increase() const noexcept { ++cnt_; }
 
-  bool Decrease() const noexcept {
-    --cnt_;
-    if (cnt_ == 0) {
-      delete this;
-      return true;
-    }
-    return false;
-  }
+  bool Decrease() const noexcept { return --cnt_ == 0; }
 
-  int Count() const noexcept { return cnt_; }
+  std::int32_t Count() const noexcept { return cnt_; }
 
   ~RefCnt() { std::cout << "Bye-bye~\n"; }
 
 private:
-  int mutable cnt_;
+  template <typename U> friend class Pointer;
+  std::int32_t mutable cnt_;
 };
 
 class A : public RefCnt<A> {
 public:
   A() noexcept {}
+
+private:
+  ~A();
 };
 
 template <typename Type, typename... Args>
-auto MakeRefCnt(Args &&...args) -> Pointer<Type> {
-  return Pointer<Type>(new Type(std::forward<Args>(args)...));
+auto MakeRefCnt(Args &&...args) -> Reference<Type> {
+  return Reference<Type>(new Type(std::forward<Args>(args)...));
+}
+
+template <typename Type, typename... Args>
+auto MakeReference(Args &&...args) -> Reference<Type> {
+  return Reference<Type>(std::forward<Args>(args)...);
+}
+
+template <typename Type> auto WrapRefCnt(Type const &type) -> Reference<Type> {
+  return Reference<Type>{&type};
 }
 } // namespace base
 
 int main(int argc, char **argv) {
-  base::Pointer<base::A> ptr = base::MakeRefCnt<base::A>();
+  base::Reference<base::A> ptr = base::MakeRefCnt<base::A>();
   assert(ptr->Count() == 1);
+  ptr->Increase();
   ptr->Decrease();
   // Oops
   std::cout << (*ptr).Count() << std::endl;
+  base::A *a = new base::A();
 }
