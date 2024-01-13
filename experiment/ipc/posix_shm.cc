3aabcdff7c4d9616e80779c1ec17662abba1216d
lipingan <lipingan.dev@outlook.com>
2024-01-13 21:29:02 +0800

    Add posix shared memory.

--- experiment/ipc/posix_shm.cc
+++ experiment/ipc/posix_shm.cc
@@ -9,6 +9,88 @@
  *
  */
 
-#include "testing/googletest/include/gtest/gtest.h"
+#include <cassert>
+#include <fcntl.h>
+#include <pthread.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-TEST(A, B) {}
+#include <cerrno>
+#include <cstdlib>
+
+#include "experiment/helper.h"
+
+char const shared_memory_name[] = "/shm.lpa";
+
+struct CountBlock {
+  int count;
+  pthread_mutex_t mutex;
+};
+
+int Server(CountBlock *count_block, int loop) {
+  for (int i = 0; i < loop; ++i) {
+    pthread_mutex_lock(&(count_block->mutex));
+    ++(count_block->count);
+    pthread_mutex_unlock(&(count_block->mutex));
+  }
+  return 0;
+}
+int Client(CountBlock *count_block, int loop) {
+  for (int i = 0; i < loop; ++i) {
+    pthread_mutex_lock(&(count_block->mutex));
+    ++(count_block->count);
+    pthread_mutex_unlock(&(count_block->mutex));
+  }
+  return 0;
+}
+
+int main() {
+  shm_unlink(shared_memory_name);
+  int oflag = O_RDWR | O_CREAT | O_TRUNC;
+  mode_t mode = 0755;
+  int mmap_fd;
+  if ((mmap_fd = shm_open(shared_memory_name, oflag, mode)) == -1) {
+    LOG_ERR_MSG();
+    std::exit(errno);
+  }
+  CountBlock count_block;
+  count_block.count = 0;
+  pthread_mutexattr_t mutex_attr;
+  pthread_mutexattr_init(&mutex_attr);
+  // This is the point.
+  // https://www.gonwan.com/2014/04/10/sharing-mutex-and-condition-variable-between-processes/
+  pthread_mutexattr_setpshared(&mutex_attr, PTHREAD_PROCESS_SHARED);
+  pthread_mutex_init(&(count_block.mutex), &mutex_attr);
+  pthread_mutexattr_destroy(&mutex_attr);
+  write(mmap_fd, &count_block, sizeof(count_block));
+  CountBlock *cb = static_cast<CountBlock *>(mmap(nullptr, sizeof(count_block),
+                                                  PROT_READ | PROT_WRITE,
+                                                  MAP_SHARED, mmap_fd, 0));
+  if (!cb) {
+    LOG_ERR_MSG();
+  }
+
+  int loop = 100'000'000;
+  pid_t child_pid = fork();
+  if (child_pid == -1) {
+    LOG_ERR_MSG();
+    std::exit(errno);
+  } else if (child_pid == 0) {
+    Server(cb, loop);
+  } else {
+    Client(cb, loop);
+    waitpid(child_pid, nullptr, 0);
+
+    if (cb->count != 2 * loop) {
+      std::cout << "Error count = " << cb->count << ", is not equal to "
+                << 2 * loop << std::endl;
+    } else {
+      std::cout << "Count = " << cb->count << ", the result is OK" << std::endl;
+    }
+    munmap(cb, sizeof(CountBlock));
+    shm_unlink(shared_memory_name);
+    return 0;
+  }
+}
